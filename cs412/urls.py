d90c7ccb9116a1f49470977185bb17e6c57245bc
jianying <jianying@bu.edu>
2024-09-24 12:41:41 -0400

    Fixed css issue

--- cs412/urls.py
+++ cs412/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from quotes import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.quotes, name='home'),
     path('quotes/', include('quotes.urls')),
-]
+] + static(settings.STATIC_URL,
+           document_root=settings.STATIC_ROOT)
