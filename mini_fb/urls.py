7c4716e5a9ba95c3f4748eda482267a08f52256d
jianying <jianying@bu.edu>
2024-10-05 18:28:27 -0400

    Added views and template for mini_fb

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from django.conf import settings
 from . import views
+from .views import ShowAllProfilesView
 
-urlpatterns = []
+# all of the URLs that are part of this app
+urlpatterns = [
+    path(r'', ShowAllProfilesView.as_view(), name='show_all_profiles'),
+]
