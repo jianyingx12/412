20056773978e7ae16fd62bb8c9ba6ce7afb5d32d
jianying <jianying@bu.edu>
2024-10-14 17:42:26 -0400

    just added comments

--- mini_fb/forms.py
+++ mini_fb/forms.py
@@ -2,11 +2,13 @@ from django import forms
 from .models import Profile, StatusMessage
 
 class CreateProfileForm(forms.ModelForm):
+    '''A form to add a profile to the database'''
     class Meta:
         model = Profile
         fields = ['first_name', 'last_name', 'city', 'bio', 'profile_image_url']
 
 class CreateStatusMessageForm(forms.ModelForm):
+    '''A form to add a statusmessage on an profile to the database'''
     class Meta:
         model = StatusMessage
         fields = ['message']
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -7,16 +7,19 @@ from django.urls import reverse
 # Create your views here.
 
 class ShowAllProfilesView(ListView):
+    '''the view to show all profiles'''
     model = Profile
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles'
 
 class ShowProfilePageView(DetailView):
+    '''Display one profile selected'''
     model = Profile
     template_name = 'mini_fb/show_profile.html'
     context_object_name = 'profile'
 
 class CreateProfileView(CreateView):
+    '''Create a profile'''
     model = Profile
     form_class = CreateProfileForm
     template_name = 'mini_fb/create_profile_form.html'
@@ -25,19 +28,32 @@ class CreateProfileView(CreateView):
         return reverse('show_profile', kwargs={'pk': self.object.pk})
     
 class CreateStatusMessageView(CreateView):
+    '''
+    A view to create a status message on a profile.
+    on GET: send back the form to display
+    on POST: read/process the form, and save new Comment to the database
+    '''
     model = StatusMessage
     form_class = CreateStatusMessageForm
     template_name = 'mini_fb/create_status_form.html'
 
+    # get the context data from the sueprclass
     def get_context_data(self, **kwargs):
+        # find the profile identified by the PK from the URL pattern
+        # add the profile referred to by the URL into this context
         context = super().get_context_data(**kwargs)
         context['profile'] = Profile.objects.get(pk=self.kwargs['pk'])
         return context
 
     def form_valid(self, form):
+        
+        # find the profile identified by the PK from the URL pattern
         profile = Profile.objects.get(pk=self.kwargs['pk'])
+        # attach this profile to the instance of the Comment to set its FK
         form.instance.profile = profile
+        # delegate work to superclass version of this method
         return super().form_valid(form)
 
     def get_success_url(self):
+        # find the profile identified by the PK from the URL pattern
         return reverse('show_profile', kwargs={'pk': self.kwargs['pk']})
